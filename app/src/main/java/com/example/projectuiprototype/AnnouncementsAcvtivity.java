b3ba901a60808337724c30dcb68ccad38d830cc1
mrcodink <shawnviera741@gmail.com>
2025-10-05 23:13:07 -0400

    Added announcment activity xml

--- app/src/main/java/com/example/projectuiprototype/AnnouncementsAcvtivity.java
+++ app/src/main/java/com/example/projectuiprototype/AnnouncementsAcvtivity.java
@@ -14,7 +14,7 @@ public class AnnouncementsAcvtivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_announcement_acvtivity);
+        setContentView(R.layout.activity_announcements_acvtivity);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
