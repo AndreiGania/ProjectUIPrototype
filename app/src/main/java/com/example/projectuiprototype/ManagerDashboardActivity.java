157673f441e2bb6a4276d928dbb867599eaa406c
kirkdouglas9000 <kirkdouglas400@gmail.com>
2025-10-05 22:34:42 -0400

    added manager dashboard code

--- app/src/main/java/com/example/projectuiprototype/ManagerDashboardActivity.java
+++ app/src/main/java/com/example/projectuiprototype/ManagerDashboardActivity.java
@@ -1,6 +1,8 @@
 package com.example.projectuiprototype;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +17,17 @@ public class ManagerDashboardActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_manager_dashboard);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.cardKpis), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        Button postBtn = findViewById(R.id.btnPostAnnouncement);
+        postBtn.setOnClickListener(v -> {
+            Intent i = new Intent(ManagerDashboardActivity.this, PostAnnouncementActivity.class);
+            startActivity(i);
+        });
+
     }
+
 }
