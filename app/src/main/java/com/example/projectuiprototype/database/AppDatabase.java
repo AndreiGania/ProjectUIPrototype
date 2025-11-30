2b7f58c1c4739d548841f263be21e16f308670e3
AndreiGania <andreijhosephgania@gmail.com>
2025-11-30 11:22:26 -0500

    dao added

---
+++ app/src/main/java/com/example/projectuiprototype/database/AppDatabase.java
@@ -0,0 +1,20 @@
+package com.example.projectuiprototype.database;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+import com.example.projectuiprototype.models.User;
+import com.example.projectuiprototype.models.InventoryItem;
+import com.example.projectuiprototype.models.Announcement;
+import com.example.projectuiprototype.models.Shift;
+
+import com.example.projectuiprototype.dao.*;
+
+@Database(entities = {User.class, InventoryItem.class, Announcement.class, Shift.class}, version = 1, exportSchema = false)
+public abstract class AppDatabase extends RoomDatabase {
+
+    public abstract UserDao userDao();
+    public abstract InventoryDao inventoryDao();
+    public abstract AnnouncementDao announcementDao();
+    public abstract ShiftDao shiftDao();
+}
