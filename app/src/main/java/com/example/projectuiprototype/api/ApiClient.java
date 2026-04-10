91a45cef6da75c2605f05574aebb81d0819fc40d
AndreiGania <andreijhosephgania@gmail.com>
2026-04-10 13:04:36 -0400

    logo

--- app/src/main/java/com/example/projectuiprototype/api/ApiClient.java
+++ app/src/main/java/com/example/projectuiprototype/api/ApiClient.java
@@ -13,7 +13,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
 
-    private static final String BASE_URL = "http://10.0.2.2:3000/";
+    private static final String BASE_URL =  "https://pointsevenbackend.onrender.com/";
     private static Retrofit retrofit;
 
     public static Retrofit getClient(Context context) {
